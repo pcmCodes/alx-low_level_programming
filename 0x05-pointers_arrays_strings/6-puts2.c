189d6a844aa0de4a047cc0ef3a3d480802c4c8c7
pcmCodes <101391794+pcmCodes@users.noreply.github.com>
2022-04-20 21:58:11 +0200

    6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,20 @@
+#include "main.h"
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+int a;
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a % 2 == 0)
+_putchar (str[a]);
+}
+
+_putchar ('\n');
+}
