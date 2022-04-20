85c426be289f669a7b08a5156c570b968079ce5b
pcmCodes <101391794+pcmCodes@users.noreply.github.com>
2022-04-20 21:59:38 +0200

    8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,30 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+
+{
+int i;
+
+i = 0;
+while (i < n)
+{
+printf("%d", a[i]);
+
+if (i < n - 1)
+{
+printf(", ");
+}
+
+i++;
+}
+
+printf("\n")
+;
+}
