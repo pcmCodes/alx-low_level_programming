80c80f168a95f1416ee9c5cf93b836a14a37e9e0
pcmCodes <pcmmogane@gmail.com>
2022-04-24 06:26:33 -0700

    My Commit

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,22 +4,22 @@
  * concatenating two strings
  *
  */
-{
+
 char *_strcat(char *dest, char *src);
-    int dently = 0, sour =0
+{
+	int dently = 0;
+	int sour = 0;
 
-    while (dest[dently] != '\0')
-	         dently++;
-
-    while (src[sour] != '\0')
-	         Sour++;
-	
-		dest[dently] = src[sour];
+	while (dest[dently] != '\0') {
 		dently++;
+	}
+	while (src[sour] != '\0') {
+		dest[dently] = src[sour];
 		sour++;
+		dently++;
+	}
 
-
-	src[dently] = '\0';
-
+	dest[dently] = '\0';
 	return (dest);
-} 
+}
+
