23a5695f1f1cb2ec5f965fd3ab6c7b680243fa65
pcmCodes <pcmmogane@gmail.com>
2022-04-24 06:15:47 -0700

    My Commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * concatenating two strings
+ *
+ */
+{
+char *_strcat(char *dest, char *src);
+    int dently = 0, sour =0
+
+    while (dest[dently] != '\0')
+	         dently++;
+
+    while (src[sour] != '\0')
+	         Sour++;
+	
+		dest[dently] = src[sour];
+		dently++;
+		sour++;
+
+
+	src[dently] = '\0';
+
+	return (dest);
+} 
