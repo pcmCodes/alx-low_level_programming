0d6f118c8a244064daa61c7390b78181a7fe46fc
pcmCodes <pcmmogane@gmail.com>
2022-04-24 14:51:21 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strncat - function concatenating two strings,
+ * @dest: end
+ * @src: origin
+ * @n: n bytes from src
+ * Return: the pointer to dest.
+ */
+char *_strncat(char *dest, char *src, int n);
+{
+	int i = 0;
+	int j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+	while (j < n)
+	{
+		*(dest + i) = *(src + j);
+		if (*(src + j) == '\0')
+			break;
+		i++;
+		j++;
+	}
+	return (dest);
+}
+
