ca4fb7b2dc2e527c627598b92eb67d4aaa36c485
pcmCodes <pcmmogane@gmail.com>
2022-04-24 16:31:46 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-        int a = 0, b = 0;
+	int a = 0, b = 0;
 
-        while (dest[a] != '\0')
-                a++;
-
-        while (src[b] != '\0')
-        {
-                dest[a] = src[b];
-                a++;
-                b++;
-        }
-
-        dest[a] = '\0';
-
-        return (dest);
+	while (dest[a] != '\0')
+		a++;
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
 }
