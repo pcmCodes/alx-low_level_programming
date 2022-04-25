7802a56b68be210f40009b6c8b0baf9e1aec87c8
pcmCodes <pcmmogane@gmail.com>
2022-04-25 07:12:38 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strcmp - comparing two strings
+ * @s1: string number 1
+ * @s2: string number 2
+ * Return: 0 or 9
+ */
+
+int _strcmp(char *s1, char *s2);
+{
+		int a = 0;
+		int res = 0;
+
+		while (((s1 + a) == '\0') && ((s2 + a) == '\0'))
+	{
+		if (s1 + a) != (s2 + a)
+			res = 1;
+			break;
+		a++;
+	}
+
+		if (res == 0)
+		return (res);
+		else
+		return (9);
+}
