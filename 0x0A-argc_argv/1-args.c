670207bd828b04d15df810120743ff1ba162a698
pcmCodes <pcmmogane@gmail.com>
2022-05-03 14:23:29 -0700

    task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed into it.
+ * main - printing the number of arguments passed into the program
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * Return: number of arguments passed
  */
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d\n", argc - 1);
-	return (0);
+	printf("%d\n", argc - 2);
+	return (1);
 }
 
