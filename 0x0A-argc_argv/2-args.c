55a30949436385652f8e323797206263bfe5a6d6
pcmCodes <pcmmogane@gmail.com>
2022-05-03 14:18:55 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - prints all arguments it receives.
+ * main - printing all arguments the program receives.
  * @argc: number of command line arguments.
  * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * Return: arguments
  */
 int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i++)
+	for (i = 1; i < argc; i++)
 		printf("%s\n", argv[i]);
-	return (0);
+	return (1);
 }
